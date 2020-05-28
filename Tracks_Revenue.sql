5516118682ef52f4ee6321296c010a3f61091921
Sampath <sampath.gonnuru@gmail.com>
2020-05-28 06:48:12 -0400

    Revenue per Track

---
+++ Tracks_Revenue.sql
@@ -0,0 +1,16 @@
+SELECT tracks.TrackId, tracks.name as Track_Name, genres.name as Genre_Name, 
+	    albums.Title as Album_Name, sum(invoice_items.UnitPrice * invoice_items.Quantity) as Revenue
+from tracks
+
+join albums
+	on tracks.AlbumId = albums.AlbumId
+
+join invoice_items
+	on tracks.TrackId = invoice_items.TrackId
+
+join genres
+	on tracks.GenreId = genres.GenreId
+	
+GROUP BY invoice_items.TrackId, albums.AlbumId, genres.GenreId
+ORDER BY sum(invoice_items.UnitPrice) DESC
+limit 100;
