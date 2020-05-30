90c3aac46bc7cb31927bd5cbeb90fdeb4ca92d7a
Sampath <sampath.gonnuru@gmail.com>
2020-05-29 22:17:54 -0400

    no of customers per employee

---
+++ customers_supported.sql
@@ -0,0 +1,12 @@
+select employees.FirstName, employees.LastName,customers.SupportRepId, employees.EmployeeId, COUNT(DISTINCT customers.CustomerId) 'Customers Supported',
+	   sum(invoices.Total) / count(DISTINCT customers.CustomerId) as 'Average Revenue',
+	   sum(invoices.Total) as 'Total Revenue'
+FROM customers
+
+join employees
+	on employees.EmployeeId = customers.SupportRepId
+	
+JOIN invoices
+	on invoices.CustomerId = customers.CustomerId
+	
+GROUP BY customers.SupportRepId;
