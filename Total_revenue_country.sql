1ba26013851a3db7f2a756158abf531913f5dd9a
Sampath <sampath.gonnuru@gmail.com>
2020-06-02 12:51:37 -0400

    Added Question as a comment

--- Total_revenue_country.sql
+++ Total_revenue_country.sql
@@ -1,3 +1,5 @@
+/* Which track generated the most revenue? which album? which genre? */
+
 SELECT invoices.BillingCountry, round(sum(invoice_items.UnitPrice) * 100 / (SELECT sum(invoice_items.UnitPrice)FROM invoice_items), 2) as '% of Total Revenue for each country' 
 FROM invoices
 join invoice_items
