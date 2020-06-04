5c021c73bd408e946d1e8f3b0b31d77555fbf936
Sampath <sampath.gonnuru@gmail.com>
2020-06-04 11:11:30 -0400

    Update customers_supported.sql

--- customers_supported.sql
+++ customers_supported.sql
@@ -1,3 +1,5 @@
+/* number of Customers supported per Employee */
+
 select employees.FirstName, employees.LastName,customers.SupportRepId, employees.EmployeeId, COUNT(DISTINCT customers.CustomerId) 'Customers Supported',
 	   sum(invoices.Total) / count(DISTINCT customers.CustomerId) as 'Average Revenue',
 	   sum(invoices.Total) as 'Total Revenue'
